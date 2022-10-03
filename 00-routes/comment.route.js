20f8912c87a835ecb2347ab2fe10f66f9d3b821c
Alhaimouni <m.alhaumouni@gmail.com>
2022-10-03 21:52:13 +0300

    edit username

--- 00-routes/comment.route.js
+++ 00-routes/comment.route.js
@@ -1,16 +1,31 @@
 'use strict';
 
 const express = require('express');
-const { commentModel } = require('../03-models');
+const { commentModel, userModel } = require('../03-models');
+const { acl } = require('../01-middlewares/acl');
+const { bearerAuth } = require('../01-middlewares/bearerAuth');
 const router = express.Router();
 
 
 
-router.post('/comment/:postID/:userID', addComment);
+router.post('/comment/:postID/:userID', bearerAuth, acl('create'), addComment);
+
+router.get('/comment/:postID', bearerAuth, acl('read'), getCommentsForPost);
 
 
 
 //===== handlers =====//
+
+
+
+async function getCommentsForPost(req, res, next) {
+  try {
+    let commentsForId = await commentModel.findAll({ where: { postId: req.params.postID }, include: userModel });
+    res.status(201).send(commentsForId);
+  } catch (err) {
+    next(`Error inside getCommentsForPost function : ${err}`);
+  }
+}
 
 
 async function addComment(req, res, next) {
